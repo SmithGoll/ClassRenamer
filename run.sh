2c5c8b83cdde0fcbb7d24ad62d48ed2a26d05e91
SmithGoll <2022209065@qq.com>
2025-06-04 22:36:38 +0800

    Fix incorrect main entry

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$(realpath ""$0"")"
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 
-exec java -cp "${SCRIPT_DIR}/build":"${SCRIPT_DIR}/lib/asm-9.6.jar" CMain "$@"
+exec java -cp "${SCRIPT_DIR}/build":"${SCRIPT_DIR}/lib/asm-9.6.jar" ClassRenamer.CMain "$@"
